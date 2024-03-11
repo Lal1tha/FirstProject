935c81e23c23a6f248ff225e6ef12dbbdce600ee
Lalitha N <155943342+Lal1tha@users.noreply.github.com>
2024-03-11 12:39:20 +0530

    Update add.c

--- add.c
+++ add.c
@@ -9,9 +9,9 @@ int main
     printf("Enter the second number: ");
     scanf("%d", &num2);
     
-    sum = num; 
+  
     
     printf("The sum of %d and %d is %d.", num1, num2,;
     
-    return(0);
+   
 }
