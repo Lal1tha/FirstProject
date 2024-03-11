70aab7c2a01435efb820465c38fcf5a3814011c9
Lal1tha <lalitha19n@gmail.com>
2024-03-11 12:35:37 +0530

    first repo

---
+++ add.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+int main
+    int num1, num2,;
+    
+    printf("Enter the first number:
+    scanf("%d", &num1);
+    
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    sum = num; 
+    
+    printf("The sum of %d and %d is %d.", num1, num2,;
+    
+    return
+}
